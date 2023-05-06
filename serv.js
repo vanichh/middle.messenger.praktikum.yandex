5d463ea6f73391efb35a8a58c832692fb2a6f230
vanich <vanich7@mail.ru>
2023-05-06 15:39:14 +0300

    fix serv

--- serv.js
+++ serv.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 
 app.use(express.static(__dirname + '/build/'));
 
+app.get('/.*/', (req, res) => {
+  return res.sendFile('./src/page/index.hbs');
+});
+
 app.listen(PORT, () => {
-  console.log('start success!');
+  console.log(`start success in port http://localhost:${PORT}`);
 });
